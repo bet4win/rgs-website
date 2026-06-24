64eb17cb9be7e6b0ca380465b0a2497daef169ca
unknown <unknown@unknown.com>
2026-06-24 14:08:26 +0400

    Add new game "Crash" and update thumbnail images for existing games in games.js

--- data/games.js
+++ data/games.js
@@ -1,90 +1,90 @@
 export const games = [
   {
+    id: "15",
+    category: "Originals",
+    title: "Crash",
+    image: "/assets/img/icons/thumbnails3/crash.jpg",
+    status: "active",
+    url: "https://remote-gaming-dev.systems.bet4.win/api/launch?game=a444355ce84b419ea48869d9c15734ab&token=DEMO&operator=5e41c28de3724d1290bbafbf6ee31cee&lang=en&site=bet4.win&branding=test",
+  },
+  {
     id: "1",
     category: "Originals",
     title: "Mines",
-    image: "/assets/img/icons/thumbnails2/mines.png",
+    image: "/assets/img/icons/thumbnails3/mines.jpg",
     status: "active",
-    url: "https://remote-gaming-dev.systems.bet4.win/api/launch?game=9943920c44b211f0be34cdfe93e2b2d7&token=DEMO&operator=5e41c28de3724d1290bbafbf6ee31cee&lang=en&site=bet4.win"
+    url: "https://remote-gaming-dev.systems.bet4.win/api/launch?game=9943920c44b211f0be34cdfe93e2b2d7&token=DEMO&operator=5e41c28de3724d1290bbafbf6ee31cee&lang=en&site=bet4.win",
   },
   {
     id: "2",
     category: "Originals",
     title: "Plinko",
-    image: "/assets/img/icons/thumbnails2/plinko.png",
+    image: "/assets/img/icons/thumbnails3/plinko.jpg",
     status: "active",
-    url: "https://remote-gaming-dev.systems.bet4.win/api/launch?game=78106d6ed4d247fbb7ac517ad8aa40d5&token=DEMO&operator=5e41c28de3724d1290bbafbf6ee31cee&lang=en&site=bet4.win"
+    url: "https://remote-gaming-dev.systems.bet4.win/api/launch?game=78106d6ed4d247fbb7ac517ad8aa40d5&token=DEMO&operator=5e41c28de3724d1290bbafbf6ee31cee&lang=en&site=bet4.win",
   },
   {
     id: "4",
     category: "Originals",
     title: "Dice",
-    image: "/assets/img/icons/thumbnails2/dice.png",
+    image: "/assets/img/icons/thumbnails3/dice.jpg",
     status: "active",
-    url: "https://remote-gaming-dev.systems.bet4.win/api/launch?game=5dcfc14083094c6b963d0dc6ad82ba68&token=DEMO&operator=5e41c28de3724d1290bbafbf6ee31cee&lang=en&site=bet4.win"
+    url: "https://remote-gaming-dev.systems.bet4.win/api/launch?game=5dcfc14083094c6b963d0dc6ad82ba68&token=DEMO&operator=5e41c28de3724d1290bbafbf6ee31cee&lang=en&site=bet4.win",
   },
   {
     id: "6",
     category: "Originals",
     title: "Wheel",
-    image: "/assets/img/icons/thumbnails2/wheel.png",
+    image: "/assets/img/icons/thumbnails3/wheel.jpg",
     status: "active",
-    url: "https://remote-gaming-dev.systems.bet4.win/api/launch?game=8d74f1b250c74e0ca003ca551ec9bd90&token=DEMO&operator=5e41c28de3724d1290bbafbf6ee31cee&lang=en&site=bet4.win"
+    url: "https://remote-gaming-dev.systems.bet4.win/api/launch?game=8d74f1b250c74e0ca003ca551ec9bd90&token=DEMO&operator=5e41c28de3724d1290bbafbf6ee31cee&lang=en&site=bet4.win",
   },
   {
     id: "7",
     category: "Originals",
     title: "Diamonds",
-    image: "/assets/img/icons/thumbnails2/diamonds.png",
+    image: "/assets/img/icons/thumbnails3/diamonds.jpg",
     status: "active",
-    url: "https://remote-gaming-dev.systems.bet4.win/api/launch?game=e07d949ed84911f08de90242ac120002&token=DEMO&operator=5e41c28de3724d1290bbafbf6ee31cee&lang=en&site=bet4.win"
+    url: "https://remote-gaming-dev.systems.bet4.win/api/launch?game=e07d949ed84911f08de90242ac120002&token=DEMO&operator=5e41c28de3724d1290bbafbf6ee31cee&lang=en&site=bet4.win",
   },
   {
     id: "8",
     category: "Originals",
     title: "Keno",
-    image: "/assets/img/icons/thumbnails2/keno.png",
+    image: "/assets/img/icons/thumbnails3/keno.jpg",
     status: "active",
-    url: "https://remote-gaming-dev.systems.bet4.win/api/launch?game=25eb025cd4bf11f08de90242ac120002&token=DEMO&operator=5e41c28de3724d1290bbafbf6ee31cee&lang=en&site=bet4.win"
+    url: "https://remote-gaming-dev.systems.bet4.win/api/launch?game=25eb025cd4bf11f08de90242ac120002&token=DEMO&operator=5e41c28de3724d1290bbafbf6ee31cee&lang=en&site=bet4.win",
   },
   {
     id: "10",
     category: "Originals",
     title: "Limbo",
-    image: "/assets/img/icons/thumbnails2/limbo.png",
+    image: "/assets/img/icons/thumbnails3/limbo.jpg",
     status: "active",
-    url: "https://remote-gaming-dev.systems.bet4.win/api/launch?game=17709360010649fa8f081e5c9920c42c&token=DEMO&operator=5e41c28de3724d1290bbafbf6ee31cee&lang=en&site=bet4.win"
+    url: "https://remote-gaming-dev.systems.bet4.win/api/launch?game=17709360010649fa8f081e5c9920c42c&token=DEMO&operator=5e41c28de3724d1290bbafbf6ee31cee&lang=en&site=bet4.win",
   },
   {
     id: "9",
     category: "Originals",
     title: "Dragon",
-    image: "/assets/img/icons/thumbnails2/placeholder.png",
+    image: "/assets/img/icons/thumbnails3/dragon.jpg",
     status: "active",
-    url: "https://remote-gaming-dev.systems.bet4.win/api/launch?game=14fdde34d95011f08de90242ac120002&token=DEMO&operator=5e41c28de3724d1290bbafbf6ee31cee&lang=en&site=bet4.win"
+    url: "https://remote-gaming-dev.systems.bet4.win/api/launch?game=14fdde34d95011f08de90242ac120002&token=DEMO&operator=5e41c28de3724d1290bbafbf6ee31cee&lang=en&site=bet4.win",
   },
   {
     id: "5",
     category: "Originals",
     title: "Hilo",
-    image: "/assets/img/icons/thumbnails2/placeholder.png",
+    image: "/assets/img/icons/thumbnails3/placeholder.jpg",
     status: "01/2026",
-    url: "https://remote-gaming-dev.systems.bet4.win/api/launch?game={}&token=DEMO&operator=5e41c28de3724d1290bbafbf6ee31cee&lang=en&site=bet4.win"
+    url: "https://remote-gaming-dev.systems.bet4.win/api/launch?game={}&token=DEMO&operator=5e41c28de3724d1290bbafbf6ee31cee&lang=en&site=bet4.win",
   },
   {
     id: "3",
     category: "Originals",
     title: "Chicken",
-    image: "/assets/img/icons/thumbnails2/placeholder.png",
+    image: "/assets/img/icons/thumbnails3/placeholder.jpg",
     status: "02/2026",
-    url: "https://remote-gaming-dev.systems.bet4.win/api/launch?game={}&token=DEMO&operator=5e41c28de3724d1290bbafbf6ee31cee&lang=en&site=bet4.win"
-  },
-  {
-    id: "15",
-    category: "Originals",
-    title: "Crash",
-    image: "/assets/img/icons/thumbnails2/placeholder.png",
-    status: "active",
-    url: "https://remote-gaming-dev.systems.bet4.win/api/launch?game=a444355ce84b419ea48869d9c15734ab&token=DEMO&operator=5e41c28de3724d1290bbafbf6ee31cee&lang=en&site=bet4.win&branding=test"
+    url: "https://remote-gaming-dev.systems.bet4.win/api/launch?game={}&token=DEMO&operator=5e41c28de3724d1290bbafbf6ee31cee&lang=en&site=bet4.win",
   },
 ];
